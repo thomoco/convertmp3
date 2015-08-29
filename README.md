204e180a9b38dd978deec95e4b6e892ef55d3c52
Thom O'Connor <thom.oconnor@yahoo.com>
2015-08-29 16:16:09 -0600

    updating to use only ffmpeg and not lame (more efficient)

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # convertmp3
 Two scripts used to convert a directory of audio files to mp3
 
+Requirements:
+ffmpeg
+
 Usage: convertdir.sh [source dir] [destination dir]
--- convertmp3.sh
+++ convertmp3.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 #
 # convertmp3.sh
 # thom o'connor
@@ -8,10 +8,23 @@
 input="$1"
 output="$2"
 
-finaloutput="${output}.mp3"
+if [ -z "${input}" -o -z "${output}" ];
+then
+ echo "Usage: convertmp3.sh [input-file] [output-directory]"
+ exit 1
+fi
 
+# create final filename 
+mydirname="`dirname \"${output}\"`"
+myfilename="`basename \"${output}\"`"
+mytype="`echo ${myfilename} | sed -e 's/^.*\.\([^.]*\)$/\1/;'`"
+finalname="${myfilename%${mytype}}mp3"
+
+# convert
 echo "Converting $input to mp3..."
-ffmpeg -i "${input}" -ac 2 -f wav - | \
-  lame -V 0 - "${finaloutput}"
+#ffmpeg -i "${input}" -ac 2 -f wav - | \
+#  lame -V 0 - "${finaloutput}"
+
+ffmpeg -i "${input}" -ac 2 -codec:a libmp3lame -qscale:a 0 "${mydirname}/${finalname}"
 
 exit 0
